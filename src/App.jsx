8532e2e96ffd16405d05c0f0fa69b555482d3fcd
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-26 01:11:10 +0200

    clear task feature is added

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,14 @@ function App() {
       };
     });
   }
-  function handleDeleteTask() {}
+  function handleDeleteTask(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
   function handleSelectProject(id) {
     setProjectState((prevProjectState) => {
       return {
@@ -103,6 +110,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectState.selectedProjectId}
       />
       {content}
     </main>
